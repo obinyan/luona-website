367e8d76daba8491f862b0f7d8747ecfab5da2ad
Emmanuel Obinyan <obinyanemmanueljunior@gmail.com>
2025-09-12 13:09:10 +0100

    pushing the hero section

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,11 @@
-import Navbar from "../components/Navbar";
+import Navbar from "@/components/Navbar";
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <main>
+    <>
       <Navbar />
-      <section className="pt-24 text-center">
-        <h1 className="text-4xl font-bold tracking-wide">
-          Welcome to Luona Luxury Shoes
-        </h1>
-        <p className="mt-4 text-gray-600">
-          Crafted elegance. Timeless style. Explore our collection.
-        </p>
-      </section>
-    </main>
+      <Hero />
+    </>
   );
 }
