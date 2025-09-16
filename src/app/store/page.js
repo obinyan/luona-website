b9bbbed315f321dad90b4e090e66aad497e4999d
Emmanuel Obinyan <obinyanemmanueljunior@gmail.com>
2025-09-16 16:18:22 +0100

    New store update

--- src/app/store/page.js
+++ src/app/store/page.js
@@ -3,6 +3,7 @@ import Storesection1 from "@/components/Storesection1";
 import Storesection2 from "@/components/Storesection2";
 import Storesection3 from "@/components/Storesection3";
 import Storesection4 from "@/components/Storesection4";
+import Storesection5 from "@/components/Storesection5";
 import Footer from "@/components/Footer";
 
 export default function Home() {
@@ -13,6 +14,7 @@ export default function Home() {
       <Storesection2 />
       <Storesection3 />
       <Storesection4 />
+      <Storesection5 />
       <Footer />
     </>
   );
